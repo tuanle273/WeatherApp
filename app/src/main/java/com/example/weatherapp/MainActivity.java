9c4518d727253c6631491b92da96fd9d96c82ef1
Anh Tuan Le <63275793+tuanle273@users.noreply.github.com>
2022-08-29 12:22:54 +0700

    add logo, component of display

--- app/src/main/java/com/example/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -3,12 +3,37 @@ package com.example.weatherapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText editSearch;
+    Button btnSearch, btnChangeActivity;
+    TextView txtName, txtCountry, txtTemp, txtStatus, txtHumidity, txtCloud, txtWind, txtDay;
+    ImageView imgIcon;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Anhxa();
+    }
+
+    private void Anhxa() {
+        editSearch = (EditText)  findViewById(R.id.edittextSearch);
+        btnSearch = (Button) findViewById(R.id.buttonSearch);
+        btnChangeActivity = (Button) findViewById(R.id.buttonChangeActivity);
+        txtName = (TextView) findViewById(R.id.textviewName);
+        txtCountry = (TextView) findViewById(R.id.textviewCountry);
+        txtTemp = (TextView) findViewById(R.id.textviewTemp);
+        txtStatus = (TextView) findViewById(R.id.textviewStatus);
+        txtHumidity = (TextView) findViewById(R.id.textViewHumidity);
+        txtCloud = (TextView) findViewById(R.id.textViewCloud);
+        txtWind = (TextView) findViewById(R.id.textViewWind);
+        txtDay = (TextView) findViewById(R.id.textviewDay);
+
     }
 }
